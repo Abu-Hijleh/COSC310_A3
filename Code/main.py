c45d464722cbf63f59762f1ad637674fd51ca62d
ravioliasb <65798416+ravioliasb@users.noreply.github.com>
2021-03-28 22:13:12 -0500

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,21 +1,32 @@
 import Preprocessor
 import Processor
+import sys, os
 
 #We have 3 python files. In this file, main.py, we run the actual code. Functionality is imported from the other 2 files.
+
+
+def blockPrint():  # Remove all the printing onto console when chatbot is loading
+    sys.stdout = open(os.devnull, 'w')
+
+
+def enablePrint():  # Restores printing
+    sys.stdout = sys.__stdout__
+
 
 questions, responses = Preprocessor.load_corpus()
 
 print("Booting Up...")
+blockPrint()
 question_list = Processor.vectorizer(questions)
+enablePrint()
 print("The Chat Bot has loaded. Type 'goodbye' to exit")
 print("Hello. My name is Nova, the astronomy Chat Bot. Pleased to meet you")
 
-#while loop to terminate conversation
 while True:  # The Chat Bot will run until 'goodbye' is inputted
     user_input = input("Input: ").lower()
     if user_input.lower() == "goodbye":
         print("Nova: See you soon!")
         quit()
     else:
-        print("Nova:",end=' ')
-        Processor.process(user_input, question_list, responses) #calls function from Processor file.
+        print("Nova:", end=' ')
+        Processor.process(user_input, question_list, responses)
