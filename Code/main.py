eb47dece814588a129535bb8cc114c4b41061330
ravioliasb <65798416+ravioliasb@users.noreply.github.com>
2021-03-29 14:39:30 -0500

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,8 @@
 import Preprocessor
 import Processor
 import sys, os
+import tkinter
+from tkinter import *
 import nltk
 from nltk.corpus import wordnet
 
@@ -15,23 +17,57 @@ def enablePrint():  # Restores printing
 
 questions, responses = Preprocessor.load_corpus()
 
-print("Booting Up...")
-blockPrint()
-question_list = Processor.vectorizer(questions)
-enablePrint()
-print("The Chat Bot has loaded. Type 'goodbye' to exit")
-print("Hello. My name is Nova, the astronomy Chat Bot. Pleased to meet you")
-
 bye_synonyms = []
 for syn in wordnet.synsets("bye"):
     for l in syn.lemmas():
         bye_synonyms.append(l.name())
 
-while True:  # The Chat Bot will run until 'goodbye' is inputted
-    user_input = input("Input: ").lower()
-    if user_input.lower() in bye_synonyms:
-        print("Nova: See you soon!")
+
+print("Booting Up...")
+blockPrint()
+question_list = Processor.vectorizer(questions)
+enablePrint()
+print("The Chat Bot has loaded. Type 'goodbye' to exit")
+
+window = tkinter.Tk()
+#height is false so that we do not lose the fixed bar at the bottom
+window.resizable(width=True, height=False)
+window.title("Nova, the chatbot")
+scrollbar = Scrollbar(window)
+scrollbar.pack(side=RIGHT, fill=Y)
+Nova = StringVar()
+You = StringVar()
+messages = Text(window, width=100, height=20, yscrollcommand=scrollbar.set, wrap=WORD)
+messages.config(background="#008080")
+scrollbar.config(command=messages.yview())
+messages.pack(anchor='nw', expand=1, fill=BOTH)
+
+def event_enter(key):
+    Enter_pressed()
+
+
+def Enter_pressed():
+
+    input_get = You.get()
+    if input_get.lower() in bye_synonyms:
         quit()
     else:
-        print("Nova:", end=' ')
-        Processor.process(user_input, question_list, responses)
+        messages.insert(END, "You: " + '%s\n' % input_get)
+        # this gets the response from the Processor.py file and then prints them out
+        get_response = Processor.process(input_get, question_list, responses)
+        messages.insert(INSERT, "Nova: " + '%s\n' % get_response)
+        You.set('')
+        messages.see(END)
+        return "break"
+
+
+Label(window, text=" user : ").pack(side=LEFT)
+Entry(window, textvariable=You, width=10, background="white").pack(side=LEFT, anchor='nw', expand=1, fill=BOTH)
+frame = Frame(window)
+Button(window, width=10, text="Send", foreground="Black", command=Enter_pressed, relief="flat").pack(side=LEFT)
+messages.insert(END, "Nova: " + '%s\n' % "Hello. My name is Nova, the astronomy and geography Chat Bot. Pleased to meet you.")
+window.bind("<Return>", event_enter)
+frame.pack(anchor='nw', expand=1, fill=BOTH)
+window.mainloop()
+
+
